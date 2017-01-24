9f8b90cdfdfc0fb2176f52686ef09779e5d8f2d5
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2017-01-23 23:56:53 -0500

    Added first FixTheError microlab

---
+++ FixTheError/Message/JollyMessage.java
@@ -0,0 +1,11 @@
+// A jolly message is written to the screen!
+class Jollymessage 
+{
+
+   public static void main(String[] args) {
+
+     //Write the message to the terminal window
+     System.out.println("Ho Ho Ho!");
+
+   }
+}
---
+++ FixTheError/README.md
@@ -0,0 +1,5 @@
+# Fix The Error
+
+Each of the subdirectories in this directory contains a program with an error. Find and fix the error to get the programs to run properly.
+
+
