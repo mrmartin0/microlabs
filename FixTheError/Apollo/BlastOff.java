9856d399108f0eb6a46b14d2fe9d11ea72ec1989
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2017-01-24 00:03:06 -0500

    Added BlastOff FixTheError microlab

---
+++ FixTheError/Apollo/BlastOff.java
@@ -0,0 +1,12 @@
+//The shuttle can't launch without your help.
+
+public class BlastOff{
+
+    public static void main(String args[]){
+      for( int t = 10; t >= 0; t++){
+          System.out.println(t);
+      }
+      System.out.println("Liftoff!!!");
+    }
+}
+
